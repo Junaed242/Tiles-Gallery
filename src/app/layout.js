70476ca14ab4b190596414821ac5652ab8973f12
Junaed242 <abdullah56429@gmail.com>
2026-05-16 11:36:13 +0600

    theme fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="light"> 
+    <html lang="en"> 
       <body className={inter.className} suppressHydrationWarning>
         <div className="min-h-screen flex flex-col bg-base-100 text-base-content">
           <AppNavbar />
